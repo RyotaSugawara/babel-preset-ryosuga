598940ec8fa8892b806fa9c22ddc301868510e37
ryo_suga <ryo.suga.dev@gmail.com>
2016-10-10 19:55:15 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# babel-preset-ryosuga
+
+> A babel preset for transforming your Javascript for me.
+
+## Install
+```bash
+npm i git://github.com/RyotaSugawara/babel-preset-ryosuga.git -D
+```
+
+## Usage
+### Via `.babelrc` (Recommended)
+**.babelrc**
+```
+{
+  "presets": ["ryosuga"]
+}
+```
+
+### Via CLI
+```
+$ babel script.js --presets ryosuga
+```
+
+### Via Node API
+```
+require('babel-core').transform('code', {
+  presets: ['ryosuga']
+});
+```
+
---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  presets: [
+    require('babel-preset-es2015-without-strict'),
+    require('babel-preset-react')
+  ],
+  plugins: [
+    require('babel-plugin-async-to-promises'),
+    require('babel-plugin-transform-class-properties'),
+    [require('babel-plugin-transform-es2015-template-literals'), { spec: true }],
+    require('babel-plugin-transform-es3-member-expression-literals'),
+    require('babel-plugin-transform-es3-property-literals'),
+    require('babel-plugin-transform-exponentiation-operator'),
+    require('babel-plugin-transform-export-extensions'),
+    require('babel-plugin-transform-es2015-destructuring'),
+    require('babel-plugin-transform-object-rest-spread')
+  ]
+};
